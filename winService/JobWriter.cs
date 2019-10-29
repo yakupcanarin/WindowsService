09a7011bff06532789470fa06e5816f21906051b
Yakup (Jacob) Can Arın <ykpcn.arn@hotmail.com>
2019-10-29 16:04:10 +0300

    Add files via upload

---
+++ winService/JobWriter.cs
@@ -0,0 +1,23 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winService
+{
+    public class JobWriter : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            using (StreamWriter _writer = (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "//test.txt") ? File.AppendText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "//test.txt") : File.CreateText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "//test.txt"))) // here will be your document's path
+            {
+                _writer.WriteLine(DateTime.Now.ToString());
+                _writer.Flush();
+                _writer.Close();
+            }
+        }
+    }
+}
---
+++ winService/Trigger.cs
@@ -0,0 +1,61 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winService
+{
+    
+    public partial class Service1 : ServiceBase
+    {
+        public static IScheduler _scheduler;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        public void Start()
+        {
+            OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
+            _scheduler = schedulerFactory.GetScheduler(); 
+            addJobs();
+            _scheduler.Start();
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        public void addJobs()
+        {
+            try
+            {
+                var job = JobBuilder.Create<JobWriter>().Build();
+                var baslangic = DateTime.Now;
+                var trigger = TriggerBuilder.Create()
+                                .StartAt(baslangic)
+                                .WithSimpleSchedule(x => x.WithIntervalInSeconds(Convert.ToInt32(2)).RepeatForever())
+                                .Build();
+                _scheduler.ScheduleJob(job, trigger); 
+            }
+
+            catch (Exception ex)
+            {
+                
+            }
+        }
+    }
+}
